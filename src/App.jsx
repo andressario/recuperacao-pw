6cdf706ee6d7642f3f19ddee560ee3654630fc8c
Kekel7 <kellmys.sz.ribeiro@gmail.com>
2022-06-29 09:44:52 -0400

    criando projeto recuperacao-pw

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Hello from "./Hello";
+import "./App.css";
+
+function App(){
+	return(
+        <div>
+            Hello wordl
+            <Hello name="Flor" />
+        </div>
+    );
+}
+export default App;
